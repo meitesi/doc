03d93f58da3b7cad50102586dcfe51868b6b911c
meitesi <133232>
2023-01-30 01:18:24 +0800

    update config.js

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,17 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-
-push_addr=`git remote git@github.com:meitesi/doc.git --push origin` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
-commit_info=`git describe --all --always --long`
-dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
-push_branch=gh-pages # 推送的分支
-
 # 生成静态文件
-npm run build
+npm run docs:build
 
 # 进入生成的文件夹
-cd $dist_path
+cd docs/.vuepress/dist
 
 git init
 git add -A
-git commit -m "deploy, $commit_info"
-git push -f $push_addr HEAD:$push_branch
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:meitesi/doc.git master:gh-pages
 
 cd -
-rm -rf $dist_path
